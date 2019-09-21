eec1a1a6a3837633dba31ffc12305a5e0c7185e0
tek2d <sami@tek2d.com>
2019-09-21 08:58:07 +0500

    using react prop system with default props and propTypes

--- src/layout/Nabvar.js
+++ src/layout/Nabvar.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
+import propTypes from "prop-types";
 
 export class Nabvar extends Component {
+  static defaultProps = {
+    title: "Github Finder",
+    icon: "fab fa-github"
+  };
+
+  static propTypes = {
+    title: propTypes.string.isRequired
+  };
+
   render() {
     return (
       <nav className='navbar bg-primary'>
         <h1>
-          <i className='fab fa-github'></i> Github Finder
+          <i className={this.props.icon}></i> {this.props.title}
         </h1>
       </nav>
     );
