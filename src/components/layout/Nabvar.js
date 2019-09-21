295951d9104a594b47d56a1a6892bce86c9ffa23
tek2d <sami@tek2d.com>
2019-09-21 13:59:00 +0500

    refactoring components to stateless components

--- src/components/layout/Nabvar.js
+++ src/components/layout/Nabvar.js
@@ -1,25 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import propTypes from "prop-types";
 
-export class Nabvar extends Component {
-  static defaultProps = {
-    title: "Github Finder",
-    icon: "fab fa-github"
-  };
+const Nabvar = ({ title, icon }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon}></i> {title}
+      </h1>
+    </nav>
+  );
+};
 
-  static propTypes = {
-    title: propTypes.string.isRequired
-  };
+Nabvar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github"
+};
 
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={this.props.icon}></i> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Nabvar.propTypes = {
+  title: propTypes.string.isRequired,
+  icon: propTypes.string.isRequired
+};
 
 export default Nabvar;
