717d5886e12e9935c5711406a88a1a21bbeb972d
tek2d <sami@tek2d.com>
2019-09-21 08:50:47 +0500

    initial commit

---
+++ src/layout/Nabvar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class Nabvar extends Component {
+  render() {
+    return (
+      <nav className='navbar bg-primary'>
+        <h1>
+          <i className='fab fa-github'></i> Github Finder
+        </h1>
+      </nav>
+    );
+  }
+}
+
+export default Nabvar;
