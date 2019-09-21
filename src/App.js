8a15bb4c1430a3699d18d90368f5eb163361e80a
Muhammad Umar Farooq <develop.umar@gmail.com>
2019-09-21 14:46:48 +0500

    HTTP Requests and updating data using state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
+
 import "./App.css";
 import Nabvar from "./components/layout/Nabvar";
 import Users from "./components/users/Users";
 
-function App() {
-  return (
-    <div className='App'>
-      <Nabvar />
-      <div className='container'>
-        <Users />
+class App extends Component {
+  state = {
+    loading: "false",
+    users: []
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const users = await axios.get("https://api.github.com/users");
+    this.setState({ loading: false, users: users.data });
+  }
+  render() {
+    return (
+      <div className='App'>
+        <Nabvar />
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
